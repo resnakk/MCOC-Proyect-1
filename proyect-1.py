02f43e793a2df233f7a6b67aec1aee6207de0491
resnakk <masanchez3@miuandes.cl>
2019-08-07 15:20:32 -0400

    algo diferente

--- proyect-1.py
+++ proyect-1.py
@@ -2,7 +2,7 @@
 from matplotlib.pylab import *
 l = 1. #Largo dell dominio
 n = 100 #Numero de intervalos
-dx  = l/n #Discretizacion espacial
+dx  = l/n #Discretizacion espa bcial
 
 #Vector con todos los x.. puntos del espacio
 x = linspace(0, l, n + 1)
@@ -38,20 +38,21 @@ print('alpha = ', alpha)
 #Loop en el tiempo
 plot(x, u0, "k--")
 k = 0
-for k in range(5000):
+for k in range(7000):
 	t = dt*k
 	print("k = ", k, "t = ", t)
 
 	#Loop en el espacio i = 1 ... n - 1; u_k1[0] = 0; u_k1[n] = 20
-	u_k[0] = 0
-	u_k[n] = 20.
+	u_k[0] = u_k[1] 
+	
+
 
 	for i in range(1,n):
 		#Algoritmo de diferencias finitas 1-D para la difusion
 		u_k1[i] = u_k[i] + alpha*(u_k[i + 1] - 2*u_k[i] + u_k[i - 1])
 	#Avanzar en la solucion
 	u_k = u_k1
-	if k % 200 == 0:
+	if k % 50 == 0:
 		plot(x, u_k)
 
 plot(x,u0)
